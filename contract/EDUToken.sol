2ea32192162052f41fbcf7fd0be9f5104a8a8f9f
Pradise2 <165901189+Pradise2@users.noreply.github.com>
2025-09-28 09:39:47 +0100

    Update EDUToken.sol

--- contract/EDUToken.sol
+++ contract/EDUToken.sol
@@ -9,8 +9,10 @@ import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/token/ERC20/extensions/ERC20PermitUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/token/ERC20/extensions/ERC20VotesUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/utils/NoncesUpgradeable.sol";
 
 contract EDUToken is Initializable, ERC20Upgradeable, ERC20BurnableUpgradeable, PausableUpgradeable, AccessControlUpgradeable, UUPSUpgradeable, ERC20PermitUpgradeable, ERC20VotesUpgradeable {
+    // FIX: Corrected keccak26 to keccak256
     bytes32 public constant ADMIN_ROLE = keccak256("ADMIN_ROLE");
     bytes32 public constant PAUSER_ROLE = keccak256("PAUSER_ROLE");
 
@@ -46,12 +48,21 @@ contract EDUToken is Initializable, ERC20Upgradeable, ERC20BurnableUpgradeable, 
 
     function _authorizeUpgrade(address newImplementation) internal override onlyRole(ADMIN_ROLE) {}
 
-    // The following functions are overrides required by Solidity.
-    function _beforeTokenTransfer(address from, address to, uint256 amount) internal override(ERC20Upgradeable, PausableUpgradeable) {
-        super._beforeTokenTransfer(from, to, amount);
+    // The following functions are overrides required by Solidity to resolve inheritance conflicts.
+
+    function _update(address from, address to, uint256 value)
+        internal
+        override(ERC20Upgradeable, ERC20VotesUpgradeable)
+    {
+        super._update(from, to, value);
     }
-    
-    function _afterTokenTransfer(address from, address to, uint256 amount) internal override(ERC20Upgradeable, ERC20VotesUpgradeable) {
-        super._afterTokenTransfer(from, to, amount);
+
+    function nonces(address owner)
+        public
+        view
+        override(ERC20PermitUpgradeable, NoncesUpgradeable)
+        returns (uint256)
+    {
+        return super.nonces(owner);
     }
 }

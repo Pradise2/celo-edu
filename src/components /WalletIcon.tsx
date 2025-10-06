abdf8eed22271b08425df6f5989e6e803494d1fe
Pradise2 <165901189+Pradise2@users.noreply.github.com>
2025-10-06 22:57:47 +0100

    Update WalletIcon.tsx

--- src/components /WalletIcon.tsx
+++ src/components /WalletIcon.tsx
@@ -2,7 +2,7 @@
 
 import { Wallet } from "lucide-react";
 
-// A simple component to return an icon based on the wallet connector's ID
+
 export const WalletIcon = ({ connectorId, className }: { connectorId: string, className: string }) => {
     // Wagmi standard IDs: 'io.metamask', 'com.coinbase.wallet', 'walletConnect'
     switch (connectorId) {
